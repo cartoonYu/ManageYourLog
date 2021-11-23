e5262958288f0659487f08abda5d7cb73c471db2
cartoon <cartoonyu3@gmail.com>
2021-11-23 20:12:32 +0800

    add mariadb db support in test module

---
+++ ManageYourLogTest/src/test/java/org/manageyourlog/test/InitMysqlData.java
@@ -0,0 +1,49 @@
+package org.manageyourlog.test;
+
+import ch.vorburger.mariadb4j.MariaDB4jService;
+import ch.vorburger.mariadb4j.springframework.MariaDB4jSpringService;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @author cartoon
+ * @date 2021/11/21 21:46
+ */
+@Component
+public class InitMysqlData implements InitializingBean, DisposableBean {
+
+    @Value("${mariadb.baseDir}")
+    private String baseDir;
+
+    @Value("${mariadb.dataDir}")
+    private String dataDir;
+
+    @Value("${mariadb.port}")
+    private Integer port;
+
+    private MariaDB4jSpringService mariaDB4jSpringService = new MariaDB4jSpringService();
+
+    private static AtomicBoolean mariaDbIsStart = new AtomicBoolean(false);
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        if(!mariaDbIsStart.get()){
+            mariaDB4jSpringService.setDefaultBaseDir(baseDir);
+            mariaDB4jSpringService.setDefaultDataDir(dataDir);
+            mariaDB4jSpringService.setDefaultPort(port);
+            mariaDB4jSpringService.start();
+            mariaDB4jSpringService.getDB().source("script/MysqlSchema.sql");
+            mariaDbIsStart.set(true);
+        }
+
+    }
+
+    @Override
+    public void destroy(){
+        mariaDB4jSpringService.stop();
+    }
+}
---
+++ ManageYourLogTest/src/test/resources/script/MysqlSchema.sql
@@ -0,0 +1,28 @@
+drop database if exists ManageYourLog;
+create database ManageYourLog;
+use ManageYourLog;
+drop table if exists LogRecord;
+CREATE TABLE `LogRecord` (
+                             `recordId` varchar(100) NOT NULL DEFAULT '' COMMENT 'record id',
+                             `content` text COMMENT 'log content',
+                             `operatorSort` varchar(100) DEFAULT '' COMMENT 'log operator sort',
+                             `operator` varchar(50) NOT NULL DEFAULT '' COMMENT 'log operator',
+                             `logRecordSort` int NOT NULL DEFAULT '0',
+                             `indexIds` varchar(200) DEFAULT '' COMMENT 'log index id collection',
+                             `version` int NOT NULL DEFAULT '1' COMMENT 'log version',
+                             `createTime` datetime NOT NULL COMMENT 'record create time',
+                             `modifyTime` datetime NOT NULL COMMENT 'record modify time',
+                             UNIQUE KEY `LogRecord_recordId_uindex` (`recordId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+drop table if exists LogRecordIndex;
+CREATE TABLE `LogRecordIndex` (
+                                  `indexId` varchar(100) NOT NULL DEFAULT '' COMMENT 'index id',
+                                  `logRecordId` varchar(100) NOT NULL DEFAULT '' COMMENT 'mark belong to which record',
+                                  `sortId` int DEFAULT '0' COMMENT 'index sort id',
+                                  `indexValue` text COMMENT 'index value',
+                                  `version` int DEFAULT '1' COMMENT 'version',
+                                  `createTime` datetime DEFAULT NULL,
+                                  `modifyTime` datetime DEFAULT NULL,
+                                  UNIQUE KEY `LogRecordIndex_indexId_uindex` (`indexId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
